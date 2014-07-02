767ec088214d986c37ffc0974e0a9ee8eddaf304
nickbolton <bolton.nick@gmail.com>
2014-07-02 18:01:50 -0500

    RM: sometime updates

--- Emitter/EmitterCollection/PBCollectionDefaultCell.m
+++ Emitter/EmitterCollection/PBCollectionDefaultCell.m
@@ -81,6 +81,7 @@
                 
             } else {
                 
+                self.item.selected = selected;
                 [super setSelected:selected];
                 [self updateForSelectedState];
             }
